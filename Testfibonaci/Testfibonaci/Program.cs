04717abe6e0d6528d5fedf87b7835df71a799fd0
lusine <lusin.khachatryan@mail.ru>
2017-01-15 00:56:22 +0400

    com1

---
+++ Testfibonaci/Testfibonaci/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibonachi;
+
+namespace Testfibonaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fib f = new fib();
+            Console.WriteLine(f[4]);
+            Console.WriteLine(f[6]);
+        }
+    }
+}
