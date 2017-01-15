541067a19687d30565e4c51b2c08661d5c1e4928
lusine <lusin.khachatryan@mail.ru>
2017-01-15 06:09:04 +0400

    com

---
+++ vectors/vec/point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pointstract
+{
+      
+
+       public struct Point
+    {
+           public Point(float x, float y)
+               : this()
+           {
+               this.X = x;
+               this.Y = y;
+           }
+
+        public float X { get; set; }
+        public float Y { get; set; }
+       
+    }
+}
+
+
+
+   
+   
+    
---
+++ vectors/vector/vec1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pointstract;
+
+namespace vector
+{
+    public class vec1
+    {
+        public Point X;
+        public Point Y;
+      public vec1(Point x,Point y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        
+        public int GetVectorlength()
+    {
+      
+        return (int)(Math.Sqrt((Math.Pow(X.X - Y.X, 2)+(Math.Pow(X.Y - Y.Y, 2)))));
+        
+    }
+
+        
+
+        public static vec1 operator +(vec1 a,vec1  b)
+
+        {
+           
+            return new vec1(new Point(a.X.X + b.X.X, a.X.Y + b.X.Y), new Point(a.Y.X + b.Y.X, a.Y.Y + b.Y.Y));
+            
+        }
+
+
+        public static vec1 operator -(vec1 a, vec1 b)
+        {
+            
+            return new vec1(new Point(a.X.X - b.X.X, a.X.Y - b.X.Y), new Point(a.Y.X - b.Y.X, a.Y.Y - b.Y.Y));
+
+        }
+
+
+
+    }
+}
